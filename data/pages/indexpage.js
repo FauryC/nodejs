db405c15af7ad442c18ebaf9f39cb78fd4d36b58
FauryC <faurycerrato04@gmail.com>
2026-03-11 17:44:44 -0600

    first commit

---
+++ data/pages/indexpage.js
@@ -0,0 +1,22 @@
+export default {
+    "backgroundId": "110",
+    "actionMessage": "Hola Mundo desde Index",
+    "index_gallery-item": [
+        {
+            "item-img": "https://picsum.photos/id/40/200/200",
+            "item-alt": "Imágen Galería 1",
+            "item-caption": "Imágen Galería 1",
+            "item-description": "s;kndfam;flma;sfma"
+        },
+        {
+            "item-img": "https://picsum.photos/id/41/200/200",
+            "item-alt": "Imágen Galería 2",
+            "item-caption": "Imágen Galería 2"
+        },
+        {
+            "item-img": "https://picsum.photos/id/42/200/200",
+            "item-alt": "Imágen Galería 3",
+            "item-caption": "Imágen Galería 4"
+        }
+    ]
+}
